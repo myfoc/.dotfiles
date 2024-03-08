fdb515f905807fb466759838b3e154d44c8de427
CofymDD <4hmdm4jh@cofymdd.com>
2024-03-08 18:21:04 +0100

    v1.0.0

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
   let
     systemSettings = {
       system = "aarch64-linux";
-      hostname = "nixos";
+      hostname = "cofymdd";
       timezone = "Europe/Bratislava";
       locale = "en_US.UTF-8";
       version = "23.11";
@@ -38,7 +38,7 @@
         home-manager.nixosModules.home-manager {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
-          home-manager.users.${userSettings.user} = import ./home-manager/home.nix;
+          home-manager.users.userSettings.user = import ./home-manager/home.nix;
           home-manager.extraSpecialArgs = { 
             inherit systemSettings;
             inherit userSettings;
