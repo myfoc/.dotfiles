c15ff0b18f3d6f5752f239cd17fd3d45bc74e3fb
CofymDD <4hmdm4jh@cofymdd.com>
2024-03-10 22:15:14 +0100

    v1.0.0

--- home/programs/hyprland/default.nix
+++ home/programs/hyprland/default.nix
@@ -31,8 +31,8 @@
 
       general {
         gaps_in = 5
-        gaps_out = 20
-        border_size = 2
+        gaps_out = 10
+        border_size = 4
         col.active_border = rgba(ffffffff)
         col.inactive_border = rgba(595959aa)
 
@@ -41,39 +41,33 @@
         allow_tearing = false
       }
 
-      decoration {
-        rounding = 0
-
-        blur {
-          enabled = true
-          size = 3
-          passes = 1
-
-          vibrancy = 0.1696
-        }
-
-        drop_shadow = true
-        shadow_range = 4
-        shadow_render_power = 3
-        col.shadow = rgba(1a1a1aee)
-      }
+decoration {
+    rounding = 10
+    multisample_edges = true
+    
+    blur = yes
+    blur_size = 5
+    blur_passes = 1
+}
 
       animations {
         enabled = true
 
-        bezier = myBezier, 0.05, 0.9, 0.1, 1.05
+        bezier = in, 0.5, 1.2, 0.4, 1
+        bezier = wss, 0.5, 1.15, 0.4, 1
+        bezier = fader, 0.33, 1, 0.68, 1
+        bezier = out, 0, 1, 0.6, 1
 
-        animation = windows, 1, 7, myBezier
-        animation = windowsOut, 1, 7, default, popin 80%
+        animation = windows, 1, 4, in
         animation = border, 1, 10, default
-        animation = borderangle, 1, 8, default
-        animation = fade, 1, 7, default
-        animation = workspaces, 1, 6, default
+        animation = fade, 1, 7, fader
+        animation = workspaces, 1, 5, wss, slide
       }
 
       dwindle {
         pseudotile = true
         preserve_split = true
+        no_gaps_when_only = true
       }
 
       gestures {
@@ -104,22 +98,12 @@
       bind = $mainMod, 3, workspace, 3
       bind = $mainMod, 4, workspace, 4
       bind = $mainMod, 5, workspace, 5
-      bind = $mainMod, 6, workspace, 6
-      bind = $mainMod, 7, workspace, 7
-      bind = $mainMod, 8, workspace, 8
-      bind = $mainMod, 9, workspace, 9
-      bind = $mainMod, 0, workspace, 10
 
       bind = $mainMod SHIFT, 1, movetoworkspace, 1
       bind = $mainMod SHIFT, 2, movetoworkspace, 2
       bind = $mainMod SHIFT, 3, movetoworkspace, 3
       bind = $mainMod SHIFT, 4, movetoworkspace, 4
       bind = $mainMod SHIFT, 5, movetoworkspace, 5
-      bind = $mainMod SHIFT, 6, movetoworkspace, 6
-      bind = $mainMod SHIFT, 7, movetoworkspace, 7
-      bind = $mainMod SHIFT, 8, movetoworkspace, 8
-      bind = $mainMod SHIFT, 9, movetoworkspace, 9
-      bind = $mainMod SHIFT, 0, movetoworkspace, 10
 
       bind = $mainMod, mouse_down, workspace, e+1
       bind = $mainMod, mouse_up, workspace, e-1
