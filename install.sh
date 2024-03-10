e46bf92930a5443729ba455537f9c4bd24883d4a
CofymDD <4hmdm4jh@cofymdd.com>
2024-03-10 14:48:04 +0100

    v1.0.0

--- install.sh
+++ install.sh
@@ -7,6 +7,8 @@ sudo git reset --hard HEAD
 
 sudo git pull
 
+sudo chmod +x install.sh
+
 sudo rm -f ~/.dotfiles/nixos/hardware-configuration.nix
 
 sudo nixos-generate-config --show-hardware-config > ~/.dotfiles/nixos/hardware-configuration.nix
