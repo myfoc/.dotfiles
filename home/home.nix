79f7e88d9920ee06d11331004bbc3ebc38ec7aec
CofymDD <4hmdm4jh@cofymdd.com>
2024-03-11 18:05:55 +0100

    v1.0.0

--- home/home.nix
+++ home/home.nix
@@ -12,7 +12,6 @@
   home.packages = with pkgs; [
     nerdfonts
     firefox
-    foot
   ];
 
   programs.home-manager.enable = true;
--- home/programs/default.nix
+++ home/programs/default.nix
@@ -1,5 +1,6 @@
 {
   imports = [
+    ./foot
     ./hyprland
     ./rofi
     ./waybar
