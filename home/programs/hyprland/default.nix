2af0d1edc7a81a01ae885e8e4d1675bf7ce6785a
CofymDD <4hmdm4jh@cofymdd.com>
2024-03-11 16:26:23 +0100

    v1.0.0

--- home/programs/hyprland/default.nix
+++ home/programs/hyprland/default.nix
@@ -32,7 +32,7 @@
       general {
         gaps_in = 5
         gaps_out = 10
-        border_size = 4
+        border_size = 2
         col.active_border = rgba(ffffffff)
         col.inactive_border = rgba(595959aa)
 
@@ -41,14 +41,15 @@
         allow_tearing = false
       }
 
-decoration {
-    rounding = 10
-    multisample_edges = true
-    
-    blur = yes
-    blur_size = 5
-    blur_passes = 1
-}
+      decoration {
+        rounding = 10
+
+        blur {
+          enabled = false
+        }
+
+        drop_shadow = false
+      }
 
       animations {
         enabled = true
@@ -67,7 +68,6 @@ decoration {
       dwindle {
         pseudotile = true
         preserve_split = true
-        no_gaps_when_only = true
       }
 
       gestures {
@@ -98,12 +98,22 @@ decoration {
       bind = $mainMod, 3, workspace, 3
       bind = $mainMod, 4, workspace, 4
       bind = $mainMod, 5, workspace, 5
+      bind = $mainMod, 6, workspace, 6
+      bind = $mainMod, 7, workspace, 7
+      bind = $mainMod, 8, workspace, 8
+      bind = $mainMod, 9, workspace, 9
+      bind = $mainMod, 0, workspace, 10
 
       bind = $mainMod SHIFT, 1, movetoworkspace, 1
       bind = $mainMod SHIFT, 2, movetoworkspace, 2
       bind = $mainMod SHIFT, 3, movetoworkspace, 3
       bind = $mainMod SHIFT, 4, movetoworkspace, 4
       bind = $mainMod SHIFT, 5, movetoworkspace, 5
+      bind = $mainMod SHIFT, 6, movetoworkspace, 6
+      bind = $mainMod SHIFT, 7, movetoworkspace, 7
+      bind = $mainMod SHIFT, 8, movetoworkspace, 8
+      bind = $mainMod SHIFT, 9, movetoworkspace, 9
+      bind = $mainMod SHIFT, 0, movetoworkspace, 10
 
       bind = $mainMod, mouse_down, workspace, e+1
       bind = $mainMod, mouse_up, workspace, e-1
