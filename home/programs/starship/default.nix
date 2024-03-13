d11e401722c9c96d92f18a3c2348983e474c2cc3
CofymDD <4hmdm4jh@cofymdd.com>
2024-03-13 10:06:24 +0100

    v1.0.0

--- home/programs/starship/default.nix
+++ home/programs/starship/default.nix
@@ -1,14 +1,11 @@
 { config, lib, pkgs, ... }: {
   programs.starship = {
     enable = true; 
-    settings = {
-      add_newline = false;
-    };
   };
 
-  # home.file.".config/" = {
-  #   recursive = true;
-  #   source = ./config;
-  #   target = ".config/";
-  # };
+  home.file.".config/" = {
+    recursive = true;
+    source = ./config;
+    target = ".config/";
+  };
 }
