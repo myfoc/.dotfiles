040c0ce7a17d3f87f4b019675f0ed3eb3b5e0827
CofymDD <4hmdm4jh@cofymdd.com>
2024-03-13 08:17:48 +0100

    v1.0.0

--- home/programs/alacritty/default.nix
+++ home/programs/alacritty/default.nix
@@ -1,11 +1,15 @@
 { config, lib, pkgs, ... }: {
   programs.alacritty = {
     enable = true; 
+    window.dimensions = {
+      lines = 3;
+      columns = 200;
+    };
   };
 
-  home.file.".config/alacritty" = {
-    recursive = true;
-    source = ./config;
-    target = ".config/alacritty";
-  };
+  # home.file.".config/alacritty" = {
+  #   recursive = true;
+  #   source = ./config;
+  #   target = ".config/alacritty";
+  # };
 }
