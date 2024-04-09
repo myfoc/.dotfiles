a8c2e10e6aebf9f8bb463b63716ab19a0c6906fe
CofymDD <4hmdm4jh@cofymdd.com>
2024-04-09 18:57:12 +0200

    v1.0.0

--- home/programs/rofi/default.nix
+++ home/programs/rofi/default.nix
@@ -1,11 +1,4 @@
 { config, pkgs, ... }: {
-  programs.rofi = {
-    enable = true;
-  };
-  home.packages = with pkgs; [
-    tofi
-  ];
-
   home.file.".config/rofi" = {
     recursive = true;
     source = ./config;
