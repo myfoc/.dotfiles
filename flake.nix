0fb68530f3a3a544f5cee0c3ac8faa41d9dba58e
CofymDD <4hmdm4jh@cofymdd.com>
2024-05-11 22:14:46 +0200

    v1.0.0

--- flake.nix
+++ flake.nix
@@ -3,11 +3,11 @@
 
   inputs = {
     nixpkgs = {
-      url = "github:nixos/nixpkgs/nixos-23.11";
+      url = "github:nixos/nixpkgs/nixos-unstable";
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.11";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
