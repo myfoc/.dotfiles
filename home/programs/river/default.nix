8f283222c04a10bbdb88d52ac8afbca9be213347
CofymDD <4hmdm4jh@cofymdd.com>
2024-05-12 03:47:45 +0200

    v1.0.0

--- home/programs/river/default.nix
+++ home/programs/river/default.nix
@@ -1,11 +1,10 @@
 { config, lib, pkgs, ... }: {
-	wayland.windowManager.river = {
-		enable = true;
-	};
+  wayland.windowManager.river = {
+    enable = true;
+  };
 
-	home.file.".config/river" = {
-    	source = ./config/init;
-   		target = ".config/river/init";
-		executable = true;
-    };
+  home.file.".config/river/init" = {
+    source = ./config/init;
+    executable = true;
+  };
 }
