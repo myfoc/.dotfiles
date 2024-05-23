8fb8ad132dcfd1580b8c926d8041ddcf9a26b3f4
CofymDD <4hmdm4jh@cofymdd.com>
2024-05-23 19:19:34 +0200

    v1.0.0

--- home/programs/packages/default.nix
+++ home/programs/packages/default.nix
@@ -1,4 +1,6 @@
 { config, lib, pkgs, ... }: {
+  services.network-manager-applet.enable;
+
   home.packages = [
     pkgs.font-awesome
     (pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
